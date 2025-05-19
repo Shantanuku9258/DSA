15287c29778fe0051f279bd487d0873b74bb653b
Shantanu Kumar <154352335+Shantanuku9258@users.noreply.github.com>
2025-05-19 08:17:43 +0530

    monday

--- src/recursion/print.java
+++ src/recursion/print.java
@@ -1,15 +1,15 @@
 package recursion;
 
 public class print {
-    public static void printNumbers(int n) {
-        if (n > 6) {
-            return; // Base condition to stop recursion
-        }
-        System.out.println(n); // Print current number
-        printNumbers(n + 1);   // Recursive call
+    int cnt =1;
+    public void printno(int n){
+        if(cnt>3) return;
+        System.out.println(cnt);
+        cnt++;
+        printno(n);
     }
-
     public static void main(String[] args) {
-        printNumbers(1); // Start from 1
+        print p =new print();
+        p.printno(3);
     }
 }
